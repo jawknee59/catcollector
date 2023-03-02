e87900f064b444e3cc77841b741658095c7fec3e
jawkneewin <johnnytnguyen93@gmail.com>
2023-03-02 12:47:06 -0600

    add cat models in model.py

--- main_app/views.py
+++ main_app/views.py
@@ -5,6 +5,7 @@ from django.shortcuts import render
 cats = [
   {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
   {'name': 'Sachi', 'breed': 'calico', 'description': 'gentle and loving', 'age': 2},
+  {'name': 'Tubs', 'breed': 'ragdoll', 'description': 'chucky lil guy', 'age': 0},
 ]
 
 # Create your views here.
